aeadd9eba700139ad4fc6e13acac3194bfeb09dd
unknown <lab3139@etecjk.local>
2025-09-25 22:23:21 -0300

    imc

--- app/index.tsx
+++ app/index.tsx
@@ -2,23 +2,31 @@ import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Home(){
-    const [name, setName] = useState('');
-    const [profissao, setProfissao] = useState('');
+    const [altura, setAltura] = useState('');
+    const [peso, setPeso] = useState('');
+    //const [imc, setImc] = useState('');
+    const at = parseFloat(altura);
+    const pe = parseFloat(peso);
+    const im = pe / (at*at);
+
     return (
         <View style={styles.body}>
             <TextInput 
-            placeholder = "Digite seu nome:"
-            onChangeText = {setName}
+            placeholder = "Digite sua altura:"
+            onChangeText = {setAltura}
             />
             <TextInput 
-            placeholder = "Digite sua Profissao:"
-            onChangeText = {setProfissao}
+            placeholder = "Digite seu Peso:"
+            onChangeText = {setPeso}
             />
             <Text style={styles.title}>
-                Qual seu nome: {name}
+                Sua altura é: {altura}
             </Text>
              <Text style={styles.title}>
-                Qual sua profissão? {profissao}
+                Seu Peso é: {peso}
+            </Text>
+            <Text style={styles.title}>
+                Seu IMC é: {im}
             </Text>
         </View>
     );
